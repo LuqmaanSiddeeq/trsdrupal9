3ef016b3bbf6bae19d4b21a05474c2871ac37f48
LuqmaanSiddeeqLK <43641228+LuqmaanSiddeeq@users.noreply.github.com>
2021-11-29 20:01:51 +0530

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -804,8 +804,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # }
 $databases['default']['default'] = array (
   'database' => 'bestsols_dev1',
-  'username' => 'root',
-  'password' => '',
+  'username' => 'bestsols_dev1',
+  'password' => 'wZ-r93%7SV.S',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
